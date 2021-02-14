83a46ec16767b3d7502cc01ddd3d474efce42f3e
kenji7157 <kawake.kk@gmail.com>
2021-02-14 19:13:34 +0900

    view/urlの設定

--- drfapi/urls.py
+++ drfapi/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('auth/', obtain_auth_token)
 ]
